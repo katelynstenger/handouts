08690a65c89f7fc0f0ac50451c6509caee61472a
Katelyn Stenger <katelynstenger@gmail.com>
2020-07-26 21:22:34 -0400

    filled out worksheet 9

--- worksheet-9.R
+++ worksheet-9.R
@@ -9,12 +9,16 @@ file.copy('data/portal.sqlite', 'myportal.sqlite')
 library(RSQLite)
 
 # Create a connection object
-con <- ...(RSQLite::SQLite(), "myportal.sqlite")
+con <- dbConnect(RSQLite::SQLite(), 
+                 "../data/myportal.sqlite")
+
+dbListFields(con, 'species')
 
 # Read a table
 library(dplyr)
 
-species <- ...(con, 'species')
+species <- tbl(con, 'species')
+species
 
 # Upload a new table
 df <- data.frame(
@@ -22,15 +26,21 @@ df <- data.frame(
   name = c('Alice', 'Bob')
 )
 
-...(con, 'observers', df)
+dbReadTable(con, 'observers', df)
+
+## Database Characteristics
+# relational database: 
+# - primary key: one or more fields uniquely identified a record in a table
+# - foreign key: primary key from one table used in diff table to est a relationship
+# - query: collect values from tables based on relationships & others
 
 # remove existing observers table
 dbRemoveTable(con, 'observers') 
 
 # Recreate observers table
 
-...(con, 'observers', list(
-  id = '...',
+dbCreateTable(con, 'observers', list(
+  id = 'integer primary key',
   name = 'text'
 ))
 
@@ -40,17 +50,23 @@ dbRemoveTable(con, 'observers')
 df <- data.frame(
   name = c('Alice', 'Bob')
 )
+dbWriteTable(con, 'observers', df,
+             append = TRUE)
 
-...(con, 'observers', df, append = TRUE)
 
-# Try adding a new observer with existing id
+# primary keys checked BEFORE duplicates end up in the data
+# throwing an error if necessary
 df <- data.frame(
   id = c(1),
   name = c('J. Doe')
 )
-
-...(con, 'observers', df,
+dbWriteTable(con, 'observers', df,
              append = TRUE)
+
+## Foreign Keys
+# A field may also be designated as a foreign key
+# establishes a relationships bt tables, 
+# a foreign key points to some primary key from diff table
 
 # Try violating foreign key constraint
 dbExecute(con, 'PRAGMA foreign_keys = ON;')
@@ -59,65 +75,73 @@ df <- data.frame(
   month = 7,
   day = 16,
   year = 1977,
-  plot_id = '...'
+  plot_id = 'Rodent'
 )
-
 dbWriteTable(con, 'surveys', df,
              append = TRUE)
 
 # Queries
+# interacting with relational databases
+
+# RMarkdown file for sql
+# ```{sql connection = con}
+# ...
+# ```
+
 # basic queries
-...(con, "... year ... surveys")
+# SELECT & FROM are SQL keywords
+dbGetQuery(con, "SELECT year FROM surveys")
 
-dbGetQuery(con, "SELECT ... FROM ...")
+# * is a wildcard
+dbGetQuery(con, "SELECT * FROM surveys")
 
-dbGetQuery(con, "...
+dbGetQuery(con, "SELECT *
 FROM surveys")
 
 # limit query response
 dbGetQuery(con, "SELECT year, species_id
 FROM surveys
-...")
+LIMIT 4")
 
 # get only unique values
-dbGetQuery(con, "SELECT ... species_id
+dbGetQuery(con, "SELECT year, species_id
 FROM surveys")
 
-dbGetQuery(con, "SELECT ...
+dbGetQuery(con, "SELECT DISTINCT species_id
 FROM surveys")
 
 # perform calculations 
 dbGetQuery(con, "SELECT plot_id, species_id,
-  sex, ...
+  sex, weight / 1000.0
+           FROM surveys")
+
+dbGetQuery(con, "SELECT plot_id, species_id, sex,
+  weight / 1000 AS weight_kg
 FROM surveys")
 
 dbGetQuery(con, "SELECT plot_id, species_id, sex,
-  weight / 1000 ...
-FROM surveys")
-
-dbGetQuery(con, "SELECT plot_id, species_id, sex,
-  ...
-FROM surveys")
+  ROUND(weight / 1000.0, 2) AS weight_kg
+           FROM surveys")
 
 # filtering
 # hint: use alternating single or double quotes to 
 # include a character string within another
 dbGetQuery(con, "SELECT *
 FROM surveys
-... species_id ...")
+           WHERE species_id = 'DM'")
 
 dbGetQuery(con, "SELECT *
 FROM surveys
-... year ...")
+WHERE year >= 2000")
 
 dbGetQuery(con, "SELECT *
 FROM surveys
-... year ... species_id ...")
+           WHERE year >= 2000 AND species_id = 'DM'")
 
 dbGetQuery(con, "SELECT *
 FROM surveys
-WHERE ...
-  ... species_id = 'DM'")
+           WHERE (year >= 2000 OR year <= 1990)
+           AND species_id = 'DM'")
 
 # Joins
 # one to many 
@@ -133,4 +157,37 @@ FROM surveys
   ON ...
 ... species
   ON ...")
+
+## Normalized Data is Tidy
+# un-tidy data with JOINs
+# SQL "JOIN" 
+lm(weight ~ treatment,
+   data = portal)
+
+lm(weight ~ genus + treatment,
+   data = portal)
+
+## Relations
+# one-to-many
+# many-to-many
+
+# one-to-many
+dbGetQuery(con, "SELECT weight, plot_type
+FROM surveys
+JOIN plots
+  ON surveys.plot_id = plots.plot_id")
+
+# many-to-many
+dbGetQuery(con, "SELECT weight, genus, plot_type
+FROM surveys
+JOIN plots
+  ON surveys.plot_id = plots.plot_id
+JOIN species
+  ON surveys.species_id = species.species_id")
+
+
+## Summary
+# concurrency: 
+# reliability: 
+# scaleability: 
 
